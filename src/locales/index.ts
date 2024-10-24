c42e33e58be3497f87ef6aea98b1d708761c1671
xiezhenghua <xiezhenghua@sailvan.com>
2024-10-24 14:03:35 +0800

    fix: 多语言浏览器持久化

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,9 +1,11 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
 import en from './en.json';
 import zh from './zh.json';
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources: {
@@ -16,7 +18,6 @@ i18n
         label: '简体中文'
       }
     },
-    lng: "zh",
     interpolation: {
       escapeValue: false // react already safes from xss
     }
